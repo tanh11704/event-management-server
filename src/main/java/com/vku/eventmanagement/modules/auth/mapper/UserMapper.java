fd4604a0e9532f0cb509948c11e8ab860b2c16c5
Trần Phước Anh <tanh11704@gmail.com>
2026-01-05 22:19:00 +0700

    refactor: clean up imports in UserMapper for improved organization

--- src/main/java/com/vku/eventmanagement/modules/auth/mapper/UserMapper.java
+++ src/main/java/com/vku/eventmanagement/modules/auth/mapper/UserMapper.java
@@ -1,10 +1,11 @@
 package com.vku.eventmanagement.modules.auth.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 import com.vku.eventmanagement.modules.auth.dto.request.RegisterRequest;
 import com.vku.eventmanagement.modules.auth.dto.response.UserResponse;
 import com.vku.eventmanagement.modules.auth.entity.UserEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
